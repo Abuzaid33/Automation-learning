7982d97c39ce1786279a068b0609e9739a8506b6
Abuzaid khan khattak <abuzaidkhattak333@gmail.com>
2026-07-04 17:23:47 +0500

    Creating greet file to check th eworking of __name__

---
+++ Day-09/greeting.py
@@ -0,0 +1,5 @@
+def greet():
+    print("Hello, welcome to the program!")
+    
+if __name__ == "__main__":
+    greet()
--- Day-09/main.py
+++ Day-09/main.py
@@ -1,6 +1,8 @@
 from employee import Employee
 from calculator import add, subtract, multiply, divide
+from greeting import greet
 
+greet()
 sum = add(10, 5)
 difference = subtract(10, 5)
 product = multiply(10, 5)
