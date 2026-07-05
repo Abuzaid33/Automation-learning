733ba0bddcf64170908b6e69e7dbb778cb15933e
Abuzaid khan khattak <abuzaidkhattak333@gmail.com>
2026-07-05 19:07:31 +0500

    some basic operations of json

---
+++ Day-11/employee.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+# creatig data folder
+os.makedirs("Day-11/data",exist_ok=True)
+
+# Reading operation
+
+if not os.path.exists("Day-11/data/employees.json"):
+    with open("Day-11/data/employees.json","r") as file:
+        employees = json.load(file) 
+else:
+    employees = []
+
+# Adding operation 
+new_employee = {
+    "name" :"Abuzaid",
+   "department" : "IT",
+   "id" : 75 
+}
+employees.append(new_employee)
+
+# saving operation
+with open("Day-11/data/employees.json","w") as file:
+    json.dump(employees,file,indent=4)
+    
+for employee in employees:
+    if employee["id"] ==  75:
+        print(employee)
+        
+for employee in employees:
+    if employee["id"] == 101:
+        employee["department"] = "AI"
---
+++ Day-11/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+for employee in employees:
+#     if employee["id"] ==  75:
+#         print(employee)
+        
