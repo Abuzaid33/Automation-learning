8fcb07635e8d5e776fbc4616e38cf97368684783
Abuzaid khan khattak <abuzaidkhattak333@gmail.com>
2026-07-07 19:44:34 +0500

    Solving employee database system

---
+++ Day-12/employee_database.py
@@ -0,0 +1,13 @@
+# Creates employees.json
+# Adds employees
+# Searches employees
+# Updates employees
+# Deletes employees
+# Logs EVERY operation
+# Handles EVERY error
+# Use
+# Functions
+# OOP
+# JSON
+# Exception Handling
+# Logging
