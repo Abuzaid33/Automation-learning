1a2c4b2ff4d5e88f77db24efa4c5e2c8723d38e8
Abuzaid khan khattak <abuzaidkhattak333@gmail.com>
2026-07-14 21:50:57 +0500

    Multiple files in to a single zip file

--- Day-16/main.py
+++ Day-16/main.py
@@ -1,7 +1,10 @@
 
-
+import os
 import zipfile
 
-with zipfile.ZipFile("Day-16/reports/reports.zip", "w") as zip_file:
-    zip_file.write("Day-16/reports/report1.txt")
-    zip_file.write("Day-16/reports/report2.txt")
+with zipfile.ZipFile("Day-16/reports/reports.zip", "w") as backup:
+    for file in os.listdir("Day-16/reports"):
+        backup.write(
+            os.path.join("Day-16/reports", file)
+        )
+print("Backup completed")
