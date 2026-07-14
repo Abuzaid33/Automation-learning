8d28629377044e598f8ee534701793c4abfdc925
Abuzaid khan khattak <abuzaidkhattak333@gmail.com>
2026-07-14 22:02:08 +0500

    Readind and extractimg from zip file

--- Day-16/main.py
+++ Day-16/main.py
@@ -6,13 +6,17 @@
 #     zip_file.write("Day-16/reports/report1.txt")
 #     zip_file.write("Day-16/reports/report2.txt")
 
+# with zipfile.ZipFile("Day-16/reports/reports.zip", "w") as backup:
+#     for file in os.listdir("Day-16/reports"):
+#         backup.write(
+#             os.path.join("Day-16/reports", file)
+#         )
+# print("Backup completed")
 
-import os
+# import os
 import zipfile
 
-with zipfile.ZipFile("Day-16/reports/reports.zip", "w") as backup:
-    for file in os.listdir("Day-16/reports"):
-        backup.write(
-            os.path.join("Day-16/reports", file)
-        )
-print("Backup completed")
+
+with zipfile.ZipFile("Day-16/reports/reports.zip","r") as backup:
+
+    backup.extractall("Day-16/Extracted")
