b3fb3437405792e4b4358c5118f933afaa431fcd
Abuzaid khan khattak <abuzaidkhattak333@gmail.com>
2026-07-14 21:51:05 +0500

    Multiple files in to a single zip file

--- Day-16/main.py
+++ Day-16/main.py
@@ -1,4 +1,12 @@
 
+
+# import zipfile
+
+# with zipfile.ZipFile("Day-16/reports/reports.zip", "w") as zip_file:
+#     zip_file.write("Day-16/reports/report1.txt")
+#     zip_file.write("Day-16/reports/report2.txt")
+
+
 import os
 import zipfile
 
